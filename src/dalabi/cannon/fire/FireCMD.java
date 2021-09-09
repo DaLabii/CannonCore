27b3bc356340dc54d8f9ef7d15a3d375e306502d
DaLabi <81080786+DaLabii@users.noreply.github.com>
2021-09-09 15:29:40 +0300

    Update FireCMD.java

--- src/dalabi/cannon/fire/FireCMD.java
+++ src/dalabi/cannon/fire/FireCMD.java
@@ -10,37 +10,41 @@ import org.bukkit.material.Button;
 
 import dalabi.cannon.Config;
 import dalabi.cannon.GeneralUtil;
+import dalabi.cannon.Storage;
 
 public class FireCMD implements CommandExecutor {
-	
+
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (sender instanceof Player) {
-			final Player player = (Player) sender;
-			if (!player.hasPermission(Config.button_command_permission)) {
-				GeneralUtil.sendMSG(player, Config.button_no_permission_message);
+			Player player = (Player) sender;
+			if (!player.hasPermission(Config.getConfig().button_command_permission)) {
+				GeneralUtil.sendMessage(player, Config.getConfig().button_no_permission_message);
 				return false;
 			}
-			if (Fire.getButtonBlock(player.getUniqueId()) == null) {
-				GeneralUtil.sendMSG(player, Config.button_not_found_message);
+			if (Storage.getStorage().getButtonBlock(player.getUniqueId()) == null) {
+				GeneralUtil.sendMessage(player, Config.getConfig().button_not_found_message);
 				return false;
 			}
-			Block block = Fire.getButtonBlock(player.getUniqueId());
-			// found some issues with it suddenly not updating when you are not loading the chunk, no clue why though
+			Block block = Storage.getStorage().getButtonBlock(player.getUniqueId());
 			if (block.getType() == Material.STONE_BUTTON) {
 				Button button = (Button) block.getState().getData();
 				FireUtil.updateButton(block, button, 21);
-				GeneralUtil.sendMSG(player, Config.button_pressed_message.replace("%x%", String.valueOf(block.getX()))
-						.replace("%y%", String.valueOf(block.getY())).replace("%z%", String.valueOf(block.getZ())));
+				GeneralUtil.sendMessage(player,
+						Config.getConfig().button_pressed_message.replace("%x%", String.valueOf(block.getX()))
+								.replace("%y%", String.valueOf(block.getY()))
+								.replace("%z%", String.valueOf(block.getZ())));
 				return false;
 			}
 			if (block.getType() == Material.WOOD_BUTTON) {
 				final Button button = (Button) block.getState().getData();
 				FireUtil.updateButton(block, button, 31);
-				GeneralUtil.sendMSG(player, Config.button_pressed_message.replace("%x%", String.valueOf(block.getX()))
-						.replace("%y%", String.valueOf(block.getY())).replace("%z%", String.valueOf(block.getZ())));
+				GeneralUtil.sendMessage(player,
+						Config.getConfig().button_pressed_message.replace("%x%", String.valueOf(block.getX()))
+								.replace("%y%", String.valueOf(block.getY()))
+								.replace("%z%", String.valueOf(block.getZ())));
 				return false;
 			}
-			GeneralUtil.sendMSG(player, Config.button_not_found_message);
+			GeneralUtil.sendMessage(player, Config.getConfig().button_not_found_message);
 		}
 		return false;
 	}
