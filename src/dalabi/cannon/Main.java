3012c200bd8dedccf47deab2fe85802bcf951e24
DaLabi <81080786+DaLabii@users.noreply.github.com>
2021-09-09 15:31:29 +0300

    added tick counter and build helper

--- src/dalabi/cannon/Main.java
+++ src/dalabi/cannon/Main.java
@@ -2,6 +2,8 @@ package dalabi.cannon;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import dalabi.cannon.buildhelper.BuildHelperCMD;
+import dalabi.cannon.buildhelper.BuildHelperEvents;
 import dalabi.cannon.fire.FireCMD;
 import dalabi.cannon.fire.FireEvents;
 import dalabi.cannon.lever.LeverCMD;
@@ -10,29 +12,43 @@ import dalabi.cannon.magicsand.MagicsandCMD;
 import dalabi.cannon.magicsand.MagicsandEvents;
 import dalabi.cannon.magicsand.MagicsandRefillCMD;
 import dalabi.cannon.magicsand.MagicsandUtil;
+import dalabi.cannon.tickcounter.TickCounterCMD;
+import dalabi.cannon.tickcounter.TickCounterEvents;
 
 public class Main extends JavaPlugin {
+	
 	private static Main main;
 
 	public void onEnable() {
 		Main.main = this;
 		getConfig().options().copyDefaults(true);
 		saveDefaultConfig();
-		Config.reload();
+		new Config();
+		new Storage();
+		new CooldownManager();
+		Config.getConfig().reload();
 		getCommand("cannoncore").setExecutor(new CannonCoreCMD());
-		if (Config.magicsand_enabled) {
+		if (Config.getConfig().magicsand_enabled) {
 			getCommand("refill").setExecutor(new MagicsandRefillCMD());
 			getCommand("ms").setExecutor(new MagicsandCMD());
 			getServer().getPluginManager().registerEvents(new MagicsandEvents(), this);
 			MagicsandUtil.startMagicsandTasks();
 		}
-		if (Config.button_enabled) {
+		if (Config.getConfig().button_enabled) {
 			getCommand("fire").setExecutor(new FireCMD());
 			getServer().getPluginManager().registerEvents(new FireEvents(), this);
 		}
-		if (Config.lever_enabled) {
+		if (Config.getConfig().lever_enabled) {
 			getCommand("lever").setExecutor(new LeverCMD());
 			getServer().getPluginManager().registerEvents(new LeverEvents(), this);
+		}
+		if (Config.getConfig().tick_counter_enabled) {
+			getCommand("tickcounter").setExecutor(new TickCounterCMD());
+			getServer().getPluginManager().registerEvents(new TickCounterEvents(), this);
+		}
+		if(Config.getConfig().build_helper_enabled) {
+			getCommand("buildhelper").setExecutor(new BuildHelperCMD());
+			getServer().getPluginManager().registerEvents(new BuildHelperEvents(), this);
 		}
 	}
 
