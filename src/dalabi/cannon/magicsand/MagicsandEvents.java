ceacc4a2d9db63ea15d3e5a24bc907c8cdf962b2
DaLabi <81080786+DaLabii@users.noreply.github.com>
2021-09-09 15:30:55 +0300

    Update MagicsandEvents.java

--- src/dalabi/cannon/magicsand/MagicsandEvents.java
+++ src/dalabi/cannon/magicsand/MagicsandEvents.java
@@ -1,20 +1,17 @@
 package dalabi.cannon.magicsand;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.metadata.FixedMetadataValue;
 
 import dalabi.cannon.Config;
 import dalabi.cannon.GeneralUtil;
-import dalabi.cannon.Main;
+import dalabi.cannon.Storage;
 import net.md_5.bungee.api.ChatColor;
 
 public class MagicsandEvents implements Listener {
@@ -27,53 +24,59 @@ public class MagicsandEvents implements Listener {
 			return;
 		}
 		Material material = event.getBlock().getType();
-		if (material != Config.magicsand_sand_item_material && material != Config.magicsand_red_sand_item_material
-				&& material != Config.magicsand_gravel_item_material) {
+		if (material != Config.getConfig().magicsand_sand_item_material
+				&& material != Config.getConfig().magicsand_red_sand_item_material
+				&& material != Config.getConfig().magicsand_gravel_item_material) {
 			return;
 		}
 		String displayName = player.getItemInHand().getItemMeta().getDisplayName();
-		if (!displayName.contentEquals(ChatColor.translateAlternateColorCodes('&', Config.magicsand_sand_item_name))
-				&& !displayName
-						.contentEquals(ChatColor.translateAlternateColorCodes('&', Config.magicsand_red_sand_item_name))
+		if (!displayName
+				.contentEquals(ChatColor.translateAlternateColorCodes('&', Config.getConfig().magicsand_sand_item_name))
 				&& !displayName.contentEquals(
-						ChatColor.translateAlternateColorCodes('&', Config.magicsand_gravel_item_name))) {
+						ChatColor.translateAlternateColorCodes('&', Config.getConfig().magicsand_red_sand_item_name))
+				&& !displayName.contentEquals(
+						ChatColor.translateAlternateColorCodes('&', Config.getConfig().magicsand_gravel_item_name))) {
 			return;
 		}
-		if (Magicsand.getMagicsandByPlayer(event.getPlayer().getUniqueId()) == null) {
-			Set<Magicsand> msSet = new HashSet<>();
-			Magicsand magicsand = new Magicsand(event.getBlock(),
-					MagicsandUtil.translateMagicsandItemDataToMaterialID(event.getBlock().getData()),
-					MagicsandUtil.translateMagicsandItemDataToBlockData(event.getBlock().getData()), 1);
-			msSet.add(magicsand);
-			Magicsand.putFirstMagicsand(event.getPlayer().getUniqueId(), msSet);
-			event.getBlock().setMetadata("magicsand", new FixedMetadataValue(Main.getInstance(), "cannoncore"));
-			return;
-		}
-		if (Magicsand.getMagicsandByPlayer(event.getPlayer().getUniqueId()).size() >= Config.magicsand_limit) {
-			GeneralUtil.sendMSG(player, Config.magicsand_limit_reached_message);
+		if (Storage.getStorage().hasMagicsand(event.getPlayer().getUniqueId()) && Storage.getStorage()
+				.getMagicsandAmount(event.getPlayer().getUniqueId()) >= Config.getConfig().magicsand_limit) {
+			GeneralUtil.sendMessage(player, Config.getConfig().magicsand_limit_reached_message);
 			event.setCancelled(true);
 			return;
 		}
 		Magicsand magicsand = new Magicsand(event.getBlock(),
 				MagicsandUtil.translateMagicsandItemDataToMaterialID(event.getBlock().getData()),
-				MagicsandUtil.translateMagicsandItemDataToBlockData(event.getBlock().getData()), 1);
-		Magicsand.addMagicsand(event.getPlayer().getUniqueId(), magicsand);
-		event.getBlock().setMetadata("magicsand", new FixedMetadataValue(Main.getInstance(), "cannoncore"));
-
+				MagicsandUtil.translateMagicsandItemDataToBlockData(event.getBlock().getData()), 1,
+				player.getUniqueId());
+		Storage.getStorage().addToMagicsand(magicsand);
+		Storage.getStorage().addToMagicsandBlockSet(event.getBlock());
+		if (Storage.getStorage().hasMagicsand(player.getUniqueId())) {
+			int currentAmount = Storage.getStorage().getMagicsandAmount(player.getUniqueId());
+			Storage.getStorage().setMagicsandAmount(player.getUniqueId(), currentAmount + 1);
+			return;
+		}
+		Storage.getStorage().setMagicsandAmount(player.getUniqueId(), 1);
 	}
 
 	@EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
 	public void onBreak(BlockBreakEvent event) {
-		if (!event.getBlock().hasMetadata("magicsand")) {
+		if (!Storage.getStorage().isBlockInMagicsandBlockSet(event.getBlock())) {
 			return;
 		}
 		Material material = event.getBlock().getType();
-		if (material != Config.magicsand_sand_item_material && material != Config.magicsand_red_sand_item_material
-				&& material != Config.magicsand_gravel_item_material) {
+		if (material != Config.getConfig().magicsand_sand_item_material
+				&& material != Config.getConfig().magicsand_red_sand_item_material
+				&& material != Config.getConfig().magicsand_gravel_item_material) {
 			return;
 		}
-		event.getBlock().removeMetadata("magicsand", Main.getInstance());
-		MagicsandClearable.addToClearable(event.getBlock());
+		Block block = event.getBlock();
+		Storage.getStorage().addToClearable(block);
+		for (Magicsand magicsand : Storage.getStorage().getMagicsandList()) {
+			if (block.getLocation() == magicsand.getBlock().getLocation()) {
+				Storage.getStorage().addToRemovable(magicsand);
+				break;
+			}
+		}
 	}
 
 }
