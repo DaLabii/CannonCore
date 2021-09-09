cdb3a71a8b2d1c98f713a5b65f683975935378fa
DaLabi <81080786+DaLabii@users.noreply.github.com>
2021-09-09 15:29:07 +0300

    added tick counter and build helper

--- src/dalabi/cannon/Config.java
+++ src/dalabi/cannon/Config.java
@@ -7,106 +7,156 @@ import org.bukkit.Material;
 
 public class Config {
 
-	public static String cannoncore_reload_permission;
-	public static String cannoncore_reload_message;
-	public static String cannoncore_no_permission_message;
-	public static String cannoncore_invalid_args_message;
+	private static Config config;
 
-	public static boolean magicsand_enabled;
-	public static String magicsand_use_permission;
-	public static String magicsand_bypass_refill_cooldown_permission;
-	public static String magicsand_bypass_clear_cooldown_permission;
-	public static String magicsand_no_permission_message;
-	public static String magicsand_invalid_args_message;
-	public static String magicsand_limit_reached_message;
-	public static String magicsand_refilled_message;
-	public static String magicsand_cleared_message;
-	public static String magicsand_refill_cooldown_message;
-	public static String magicsand_clear_cooldown_message;
-	public static int magicsand_refill_cooldown;
-	public static int magicsand_clear_cooldown;
-	public static int magicsand_limit;
-	public static Material magicsand_sand_item_material;
-	public static byte magicsand_sand_item_data;
-	public static String magicsand_sand_item_name;
-	public static List<String> magicsand_sand_item_lore = new ArrayList<>();
-	public static Material magicsand_red_sand_item_material;
-	public static byte magicsand_red_sand_item_data;
-	public static String magicsand_red_sand_item_name;
-	public static List<String> magicsand_red_sand_item_lore = new ArrayList<>();
-	public static Material magicsand_gravel_item_material;
-	public static byte magicsand_gravel_item_data;
-	public static String magicsand_gravel_item_name;
-	public static List<String> magicsand_gravel_item_lore = new ArrayList<>();
+	public Config() {
+		config = this;
+	}
 
-	public static boolean button_enabled;
-	public static String button_command_permission;
-	public static String button_pressed_message;
-	public static String button_not_found_message;
-	public static String button_no_permission_message;
-	public static String button_invalid_args_message;
-	
-	public static boolean lever_enabled;
-	public static String lever_command_permission;
-	public static String lever_flicked_message;
-	public static String lever_not_found_message;
-	public static String lever_no_permission_message;
-	public static String lever_invalid_args_message;
+	public static Config getConfig() {
+		return config;
+	}
 
-	public static void reload() {
-		
+	public String cannoncore_reload_permission;
+	public String cannoncore_reload_message;
+	public String cannoncore_no_permission_message;
+	public String cannoncore_invalid_args_message;
+
+	public boolean magicsand_enabled;
+	public String magicsand_use_permission;
+	public String magicsand_bypass_refill_cooldown_permission;
+	public String magicsand_bypass_clear_cooldown_permission;
+	public String magicsand_no_permission_message;
+	public String magicsand_invalid_args_message;
+	public String magicsand_limit_reached_message;
+	public String magicsand_refilled_message;
+	public String magicsand_cleared_message;
+	public String magicsand_refill_cooldown_message;
+	public String magicsand_clear_cooldown_message;
+	public List<String> magicsand_help_messages;
+	public int magicsand_refill_cooldown;
+	public int magicsand_clear_cooldown;
+	public int magicsand_limit;
+	public int magicsand_refill_limit;
+	public Material magicsand_sand_item_material;
+	public byte magicsand_sand_item_data;
+	public String magicsand_sand_item_name;
+	public List<String> magicsand_sand_item_lore;
+	public Material magicsand_red_sand_item_material;
+	public byte magicsand_red_sand_item_data;
+	public String magicsand_red_sand_item_name;
+	public List<String> magicsand_red_sand_item_lore;
+	public Material magicsand_gravel_item_material;
+	public byte magicsand_gravel_item_data;
+	public String magicsand_gravel_item_name;
+	public List<String> magicsand_gravel_item_lore;
+
+	public boolean button_enabled;
+	public String button_command_permission;
+	public String button_pressed_message;
+	public String button_not_found_message;
+	public String button_no_permission_message;
+	public String button_invalid_args_message;
+
+	public boolean lever_enabled;
+	public String lever_command_permission;
+	public String lever_flicked_message;
+	public String lever_not_found_message;
+	public String lever_no_permission_message;
+	public String lever_invalid_args_message;
+
+	public boolean tick_counter_enabled;
+	public String tick_counter_ticks_added_message;
+	public String tick_counter_ticks_reset_message;
+	public String tick_counter_given_message;
+
+	public boolean build_helper_enabled;
+	public String build_helper_command_permission;
+	public String build_helper_no_permission_message;
+	public String build_helper_enabled_message;
+	public String build_helper_disabled_message;
+
+	public void reload() {
+
 		Main.getInstance().reloadConfig();
-		
-		cannoncore_reload_permission = Main.getInstance().getConfig().getString("cannoncore.permission.reload");
-		cannoncore_reload_message = Main.getInstance().getConfig().getString("cannoncore.message.config_reloaded");
-		cannoncore_no_permission_message = Main.getInstance().getConfig().getString("cannoncore.message.no_permission");
-		cannoncore_invalid_args_message = Main.getInstance().getConfig().getString("cannoncore.message.invalid_args");
-		magicsand_enabled = Main.getInstance().getConfig().getBoolean("magicsand.enabled");
-		magicsand_use_permission = Main.getInstance().getConfig().getString("magicsand.permission.use");
-		magicsand_bypass_refill_cooldown_permission = Main.getInstance().getConfig()
+
+		this.cannoncore_reload_permission = Main.getInstance().getConfig().getString("cannoncore.permission.reload");
+		this.cannoncore_reload_message = Main.getInstance().getConfig().getString("cannoncore.message.config_reloaded");
+		this.cannoncore_no_permission_message = Main.getInstance().getConfig()
+				.getString("cannoncore.message.no_permission");
+		this.cannoncore_invalid_args_message = Main.getInstance().getConfig()
+				.getString("cannoncore.message.invalid_args");
+
+		this.magicsand_enabled = Main.getInstance().getConfig().getBoolean("magicsand.enabled");
+		this.magicsand_use_permission = Main.getInstance().getConfig().getString("magicsand.permission.use");
+		this.magicsand_bypass_refill_cooldown_permission = Main.getInstance().getConfig()
 				.getString("magicsand.permission.bypass_refill_cooldown");
-		magicsand_bypass_clear_cooldown_permission = Main.getInstance().getConfig()
+		this.magicsand_bypass_clear_cooldown_permission = Main.getInstance().getConfig()
 				.getString("magicsand.permission.bypass_clear_cooldown");
-		magicsand_no_permission_message = Main.getInstance().getConfig().getString("magicsand.message.no_permission");
-		magicsand_invalid_args_message = Main.getInstance().getConfig().getString("magicsand.message.invalid_args");
-		magicsand_limit_reached_message = Main.getInstance().getConfig().getString("magicsand.message.limit_reached");
-		magicsand_refilled_message = Main.getInstance().getConfig().getString("magicsand.message.refilled");
-		magicsand_cleared_message = Main.getInstance().getConfig().getString("magicsand.message.cleared");
-		magicsand_refill_cooldown_message = Main.getInstance().getConfig()
+		this.magicsand_no_permission_message = Main.getInstance().getConfig()
+				.getString("magicsand.message.no_permission");
+		this.magicsand_invalid_args_message = Main.getInstance().getConfig()
+				.getString("magicsand.message.invalid_args");
+		this.magicsand_limit_reached_message = Main.getInstance().getConfig()
+				.getString("magicsand.message.limit_reached");
+		this.magicsand_refilled_message = Main.getInstance().getConfig().getString("magicsand.message.refilled");
+		this.magicsand_cleared_message = Main.getInstance().getConfig().getString("magicsand.message.cleared");
+		this.magicsand_help_messages = new ArrayList<>();
+		this.magicsand_help_messages.addAll(Main.getInstance().getConfig().getStringList("magicsand.message.help"));
+		this.magicsand_refill_cooldown_message = Main.getInstance().getConfig()
 				.getString("magicsand.message.refill_cooldown");
-		magicsand_clear_cooldown_message = Main.getInstance().getConfig().getString("magicsand.message.clear_cooldown");
-		magicsand_refill_cooldown = Main.getInstance().getConfig().getInt("magicsand.cooldown.refill");
-		magicsand_clear_cooldown = Main.getInstance().getConfig().getInt("magicsand.cooldown.clear");
-		magicsand_limit = Main.getInstance().getConfig().getInt("magicsand.limit");
-		magicsand_sand_item_material = Material
+		this.magicsand_clear_cooldown_message = Main.getInstance().getConfig()
+				.getString("magicsand.message.clear_cooldown");
+		this.magicsand_refill_cooldown = Main.getInstance().getConfig().getInt("magicsand.cooldown.refill");
+		this.magicsand_clear_cooldown = Main.getInstance().getConfig().getInt("magicsand.cooldown.clear");
+		this.magicsand_limit = Main.getInstance().getConfig().getInt("magicsand.limit");
+		this.magicsand_refill_limit = Main.getInstance().getConfig().getInt("magicsand.refill_limit");
+		this.magicsand_sand_item_material = Material
 				.getMaterial(Main.getInstance().getConfig().getString("magicsand.sand.item.material"));
-		magicsand_sand_item_data = (byte) Main.getInstance().getConfig().getInt("magicsand.sand.item.data");
-		magicsand_sand_item_name = Main.getInstance().getConfig().getString("magicsand.sand.item.name");
-		magicsand_sand_item_lore.addAll(Main.getInstance().getConfig().getStringList("magicsand.sand.item.lore"));
-		magicsand_red_sand_item_material = Material
+		this.magicsand_sand_item_data = (byte) Main.getInstance().getConfig().getInt("magicsand.sand.item.data");
+		this.magicsand_sand_item_name = Main.getInstance().getConfig().getString("magicsand.sand.item.name");
+		this.magicsand_sand_item_lore = new ArrayList<>();
+		this.magicsand_sand_item_lore.addAll(Main.getInstance().getConfig().getStringList("magicsand.sand.item.lore"));
+		this.magicsand_red_sand_item_material = Material
 				.getMaterial(Main.getInstance().getConfig().getString("magicsand.red_sand.item.material"));
-		magicsand_red_sand_item_data = (byte) Main.getInstance().getConfig().getInt("magicsand.red_sand.item.data");
-		magicsand_red_sand_item_name = Main.getInstance().getConfig().getString("magicsand.red_sand.item.name");
-		magicsand_red_sand_item_lore
+		this.magicsand_red_sand_item_data = (byte) Main.getInstance().getConfig()
+				.getInt("magicsand.red_sand.item.data");
+		this.magicsand_red_sand_item_name = Main.getInstance().getConfig().getString("magicsand.red_sand.item.name");
+		this.magicsand_red_sand_item_lore = new ArrayList<>();
+		this.magicsand_red_sand_item_lore
 				.addAll(Main.getInstance().getConfig().getStringList("magicsand.red_sand.item.lore"));
-		magicsand_gravel_item_material = Material
+		this.magicsand_gravel_item_material = Material
 				.getMaterial(Main.getInstance().getConfig().getString("magicsand.gravel.item.material"));
-		magicsand_gravel_item_data = (byte) Main.getInstance().getConfig().getInt("magicsand.gravel.item.data");
-		magicsand_gravel_item_name = Main.getInstance().getConfig().getString("magicsand.gravel.item.name");
-		magicsand_gravel_item_lore.addAll(Main.getInstance().getConfig().getStringList("magicsand.gravel.item.lore"));
-		
-		button_enabled = Main.getInstance().getConfig().getBoolean("button.enabled");
-		button_command_permission = Main.getInstance().getConfig().getString("button.permission.use");
-		button_pressed_message = Main.getInstance().getConfig().getString("button.message.pressed");
-		button_not_found_message = Main.getInstance().getConfig().getString("button.message.not_found");
-		button_no_permission_message = Main.getInstance().getConfig().getString("button.message.no_permission");
-		button_invalid_args_message = Main.getInstance().getConfig().getString("button.message.invalid_args");
-		
-		lever_enabled = Main.getInstance().getConfig().getBoolean("lever.enabled");
-		lever_command_permission = Main.getInstance().getConfig().getString("lever.permission.use");
-		lever_flicked_message = Main.getInstance().getConfig().getString("lever.message.flicked");
-		lever_not_found_message = Main.getInstance().getConfig().getString("lever.message.not_found");
-		lever_no_permission_message = Main.getInstance().getConfig().getString("lever.message.no_permission");
-		lever_invalid_args_message = Main.getInstance().getConfig().getString("lever.message.invalid_args");
+		this.magicsand_gravel_item_data = (byte) Main.getInstance().getConfig().getInt("magicsand.gravel.item.data");
+		this.magicsand_gravel_item_name = Main.getInstance().getConfig().getString("magicsand.gravel.item.name");
+		this.magicsand_gravel_item_lore = new ArrayList<>();
+		this.magicsand_gravel_item_lore
+				.addAll(Main.getInstance().getConfig().getStringList("magicsand.gravel.item.lore"));
+
+		this.button_enabled = Main.getInstance().getConfig().getBoolean("button.enabled");
+		this.button_command_permission = Main.getInstance().getConfig().getString("button.permission.use");
+		this.button_pressed_message = Main.getInstance().getConfig().getString("button.message.pressed");
+		this.button_not_found_message = Main.getInstance().getConfig().getString("button.message.not_found");
+		this.button_no_permission_message = Main.getInstance().getConfig().getString("button.message.no_permission");
+		this.button_invalid_args_message = Main.getInstance().getConfig().getString("button.message.invalid_args");
+
+		this.lever_enabled = Main.getInstance().getConfig().getBoolean("lever.enabled");
+		this.lever_command_permission = Main.getInstance().getConfig().getString("lever.permission.use");
+		this.lever_flicked_message = Main.getInstance().getConfig().getString("lever.message.flicked");
+		this.lever_not_found_message = Main.getInstance().getConfig().getString("lever.message.not_found");
+		this.lever_no_permission_message = Main.getInstance().getConfig().getString("lever.message.no_permission");
+		this.lever_invalid_args_message = Main.getInstance().getConfig().getString("lever.message.invalid_args");
+
+		this.tick_counter_enabled = Main.getInstance().getConfig().getBoolean("tick_counter.enabled");
+		this.tick_counter_ticks_added_message = Main.getInstance().getConfig().getString("tick_counter.message.added");
+		this.tick_counter_ticks_reset_message = Main.getInstance().getConfig().getString("tick_counter.message.reset");
+		this.tick_counter_given_message = Main.getInstance().getConfig().getString("tick_counter.message.given");
+
+		this.build_helper_enabled = Main.getInstance().getConfig().getBoolean("build_helper.enabled");
+		this.build_helper_command_permission = Main.getInstance().getConfig().getString("build_helper.permission.use");
+		this.build_helper_no_permission_message = Main.getInstance().getConfig()
+				.getString("build_helper.message.no_permission");
+		this.build_helper_enabled_message = Main.getInstance().getConfig().getString("build_helper.message.enabled");
+		this.build_helper_disabled_message = Main.getInstance().getConfig().getString("build_helper.message.disabled");
 	}
 }
