24f36b1491feb452c77c94b7ffa1ae1b45626d1d
DaLabi <81080786+DaLabii@users.noreply.github.com>
2021-09-09 15:28:53 +0300

    Update CannonCoreCMD.java

--- src/dalabi/cannon/CannonCoreCMD.java
+++ src/dalabi/cannon/CannonCoreCMD.java
@@ -10,19 +10,19 @@ public class CannonCoreCMD implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if (sender instanceof Player) {
-			final Player player = (Player) sender;
-			if (!player.hasPermission(Config.cannoncore_reload_permission)) {
-				GeneralUtil.sendMSG(player, Config.cannoncore_no_permission_message);
+			Player player = (Player) sender;
+			if (!player.hasPermission(Config.getConfig().cannoncore_reload_permission)) {
+				GeneralUtil.sendMessage(player, Config.getConfig().cannoncore_no_permission_message);
 				return false;
 			}
 			if (args.length == 1) {
 				switch (args[0].toLowerCase()) {
 				case "reload":
-					Config.reload();
-					GeneralUtil.sendMSG(player, Config.cannoncore_reload_message);
+					Config.getConfig().reload();
+					GeneralUtil.sendMessage(player, Config.getConfig().cannoncore_reload_message);
 					break;
 				default:
-					GeneralUtil.sendMSG(player, Config.cannoncore_invalid_args_message);
+					GeneralUtil.sendMessage(player, Config.getConfig().cannoncore_invalid_args_message);
 				}
 			}
 		}
